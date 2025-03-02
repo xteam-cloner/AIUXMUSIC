826fc93e47c3b607d63694f2f885fb0eb6d6360a
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-03-03 01:28:12 +0700

    Update config.py

--- config.py
+++ config.py
@@ -60,9 +60,9 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", "https://t.me/AIUCHANNEL")
+    "SUPPORT_CHANNEL", "https://t.me/xteam_cloner")
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "https://t.me/AIU_SUPPORT")
+    "SUPPORT_GROUP", "https://t.me/xteam_cloner")
 
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
