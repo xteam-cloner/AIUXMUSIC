555e678691e29a1396810d23408214056ddd071c
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-03-03 01:38:06 +0700

    Update bot.py

--- AIUXMUSIC/core/bot.py
+++ AIUXMUSIC/core/bot.py
@@ -9,7 +9,7 @@ import config
 from ..logging import LOGGER
 
 
-class xteam_musicbot(Client):
+class AIUXMUSICBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot")
         super().__init__(
